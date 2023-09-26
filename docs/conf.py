818966e3a5ecfa1b02b7bfe95a29c088881c9276
Martin Salinas <ssalinasmartin@gmail.com>
2023-09-26 16:21:47 +0200

    Favicon

--- docs/conf.py
+++ docs/conf.py
@@ -162,7 +162,10 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_static/pdfs']
+html_static_path = ['_static']
+
+# Favicon
+html_favicon = 'images/favicon.ico'
 
 # Style
 
