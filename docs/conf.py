42c520d904cd85673c6c740ad9a1356af880dd85
Martin Salinas <ssalinasmartin@gmail.com>
2023-10-09 20:11:59 +0200

    Fix path

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ templates_path = ['../_templates']
 smv_tag_whitelist = r'^.*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = None
+smv_branch_whitelist = 'master'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = None
@@ -161,7 +161,7 @@ html_theme_options = {
 html_static_path = ['../_static']
 
 # Favicon
-html_favicon = os.path.join('..', html_static_path[0], 'images', 'favicon.ico')
+html_favicon = os.path.join(html_static_path[0], 'images', 'favicon.ico')
 
 # Style
 
